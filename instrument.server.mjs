b5bf88bb4b060e1857401cd8a46d5b27b9cb0742
matslars <matslars.groeneweg@gmail.com>
2025-05-12 08:29:08 +0200

    appwrite included

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -12,3 +12,16 @@ Sentry.init({
     tracesSampleRate: 1.0, // Capture 100% of the transactions
     profilesSampleRate: 1.0, // profile every transaction
 });
+
+// Sentry.init({
+//     dsn: "https://db01fe62687c401f8ba95397eddcca2e@o4509280391069696.ingest.de.sentry.io/4509280399392848",
+//     // Setting this option to true will send default PII data to Sentry.
+//     // For example, automatic IP address collection on events
+//     sendDefaultPii: true,
+//     integrations: [
+//         Sentry.replayIntegration()
+//     ],
+//     // Session Replay
+//     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//     replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
