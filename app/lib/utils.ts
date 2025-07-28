0a5bdab009a8479c5b86923d62328bae8b7bd1a4
matslars <matslars.groeneweg@gmail.com>
2025-07-29 01:53:24 +0200

    some unfinished changes - do not pull

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export const formatDate = (dateString: string): string => {
+export const formatDate =   (dateString: string): string => {
     return dayjs(dateString).format("MMMM DD, YYYY");
 }
 
--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+// import { SfDataForm, FormValidator, DataAnnotationsValidator, FormItems, FormAutoGenerateItems } from '@syncfusion/ej2-react-dataform';
+import {createTrip} from "~/routes/api/api";
 
 const CreateTrip = () => {
+
+    const TripDetail = {
+        name: '',
+        description: '',
+        itinerary: [
+            {
+                location: '',
+                date: '',
+                description: '',
+                imageUrls: [],
+            },
+        ],
+        budget: '',
+        tags: [],
+        estimatedPrice: '',
+        imageUrl: '',
+        status: '',
+        dateCreated: '',
+        dateUpdated: '',
+        dateJoined: '',
+        itineraryCreated: '',
+        userId: '',
+    };
+
+    createTrip();
+
     return (
-        <div>CreateTrip</div>
+        <div className="create-trip">
+            <h2 className="new-trip">
+
+            </h2>
+        </div>
     )
 }
 export default CreateTrip
